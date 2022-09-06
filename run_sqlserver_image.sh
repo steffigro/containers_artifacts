1810736f7fc70d3200b15f86e208b8f089095725
carlosasantos <37413649+carlosasantos@users.noreply.github.com>
2022-09-06 13:13:58 +0100

    updates

--- run_sqlserver_image.sh
+++ run_sqlserver_image.sh
@@ -1,1 +1,1 @@
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=salkfjhs123!34dLQkf@j" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run --name sql -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=salkfjhs12334dLQkf@j" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
