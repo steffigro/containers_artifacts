441ce8283012d91735b68f7204c52513a912e645
carlosasantos <37413649+carlosasantos@users.noreply.github.com>
2022-09-07 14:55:09 +0100

    refactor repo

--- run_sqlserver_image.sh
+++ run_sqlserver_image.sh
@@ -1,1 +1,5 @@
-docker run --name sql -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=salkfjhs12334dLQkf@j" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+# Create Docker Network 
+docker network create tripviewer
+
+# Run SQL Server Container with tripviewer2 network
+docker run --network tripviewer --name sql -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=salkfjhs12334dLQkf@j" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
