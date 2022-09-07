83af664a2ceae33f36434f875192dd4e91d08f25
carlosasantos <37413649+carlosasantos@users.noreply.github.com>
2022-09-07 12:53:34 +0100

    updates

--- ch1_build_and_deploy.sh
+++ ch1_build_and_deploy.sh
@@ -1,6 +1,3 @@
-# 
-export ACR_REGISTRY=registrykcu5316
-
 docker build -f dockerfiles/Dockerfile_0 -t "registrykcu5316.azurecr.io/tripinsights/user-java:1.0" src/user-java/.
 docker build -f dockerfiles/Dockerfile_1 -t "registrykcu5316.azurecr.io/tripinsights/tripviewer:1.0" src/tripviewer/.
 docker build -f dockerfiles/Dockerfile_2 -t "registrykcu5316.azurecr.io/tripinsights/userprofile:1.0" src/userprofile/.
